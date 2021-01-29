e940a654c7d50cad7b51939e7141b131e15a5f2a
Camila Sayuri <camilahanazono@gmail.com>
2021-01-28 21:26:27 -0300

    recebendo o dado e fazendo a consulta

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios'
+import {Dados} from './types/dados'
 
 function App() {
+
+  const [CEP, setCEP] = useState<String>("")
+
+  const [dados, setDados] = useState<Dados>()
+
+  const getDados = () => {
+    axios.get(`https://viacep.com.br/ws/${CEP}/json/`)
+      .then(resposta => setDados(resposta.data))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(event) => setCEP(event.target.value)}/>
+      <button onClick={getDados}>Consultar CEP</button>
+
+      {
+        <>
+          <h1>CEP: {dados?.cep}</h1>
+          <p>Logradouro: {dados?.logradouro}</p>
+          <p>Complemento: {dados?.complemento}</p>
+          <p>Bairro: {dados?.bairro}</p>
+          <p>Localidade: {dados?.localidade}</p>
+          <p>UF: {dados?.uf}</p>
+          <p>IBGE: {dados?.ibge}</p>
+          <p>GIA: {dados?.gia}</p>
+          <p>DDD: {dados?.ddd}</p>
+          <p>SIAFI: {dados?.siafi}</p>
+        </>
+      }
+
     </div>
   );
 }
